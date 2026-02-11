002390142fa5f4d76cb02e24ba565389b60bd583
ami76 <amimari7231@gmail.com>
2026-02-11 16:29:30 +0000

    Entrega proyecto

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -1,19 +1,4 @@
--- PLEASE READ THIS BEFORE RUNNING THE EXERCISE
 
--- ⚠️ IMPORTANT: This SQL file may crash due to two common issues: comments and missing semicolons.
-
--- ✅ Suggestions:
--- 1) Always end each SQL query with a semicolon `;`
--- 2) Ensure comments are well-formed:
---    - Use `--` for single-line comments only
---    - Avoid inline comments after queries
---    - Do not use `/* */` multi-line comments, as they may break execution
-
--- -----------------------------------------------
--- queries.sql
--- Complete each mission by writing your SQL query
--- directly below the corresponding instruction
--- -----------------------------------------------
 
 SELECT * FROM regions;
 SELECT * FROM species;
@@ -21,32 +6,70 @@ SELECT * FROM climate;
 SELECT * FROM observations;
 
 
--- MISSION 1
--- Your query here;
+SELECT * FROM observations LIMIT 10;
 
--- MISSION 2
--- Your query here;
+SELECT DISTINCT region_id FROM observations;
+
+SELECT COUNT(DISTINCT species_id) AS total_species FROM observations;
+
+SELECT * FROM observations WHERE region_id = 2;
+
+SELECT * FROM observations WHERE observation_date = '1998-08-08';
+
+SELECT o.region_id, r.name AS region_name, COUNT(*) AS total
+FROM observations o
+JOIN regions r ON o.region_id = r.id
+GROUP BY o.region_id, r.name
+ORDER BY total DESC;
+
+SELECT species_id, COUNT(*) AS total
+FROM observations
+GROUP BY species_id
+ORDER BY total DESC
+LIMIT 5;
+
+SELECT species_id, COUNT(*) AS count_observations
+FROM observations
+GROUP BY species_id
+HAVING count_observations < 5
+ORDER BY count ASC;
+
+SELECT observer, COUNT(*) AS total_observations
+FROM observations
+GROUP BY observer
+ORDER BY total_observations DESC;
+
+SELECT o.id, r.name AS region_name, o.latitude, o.longitude, o.observation_date
+FROM observations o
+JOIN regions r ON o.region_id = r.id;
+
+SELECT o.id, sp.scientific_name
+FROM observations o
+JOIN species sp ON o.species_id = sp.id;
+
+SELECT r.name AS region, sp.scientific_name, COUNT(*) AS total
+FROM observations o
+JOIN species sp ON o.species_id = sp.id
+JOIN regions r ON o.region_id = r.id
+GROUP BY region, sp.scientific_name
+ORDER BY region, total DESC;
+
+-- Nivel 4 (Opcional) - 13
+INSERT INTO observations (species_id, region_id, observer, observation_date, latitude, longitude, count) 
+VALUES (5, 7, 'observer1111', '2026-02-05', -13.429111, 132.41847, 1 );
+
+--
+INSERT INTO observations (species_id, region_id, observer, observation_date, latitude, longitude, count) 
+VALUES (5, 7, 'prueba', '2026-02-05', -13.429111, 132.41847, 1 );
+
+-- 14
+UPDATE species
+SET scientific_name = 'Panthera leo'
+WHERE scientific_name = 'Pantera leo';
+
+-- 15
+DELETE FROM observations
+WHERE observer = 'prueba';
 
 
--- MISSION 3
--- Your query here;
 
-
--- MISSION 4
--- Your query here;
-
-
--- MISSION 5
--- Your query here;
-
-
--- MISSION 6
--- Your query here;
-
-
--- MISSION 7
--- Your query here;
-
-
--- MISSION 8
--- Your query here;
